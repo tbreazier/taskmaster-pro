ebbb5af433b8a7ac14b66efd73bb8043446df816
Tom Breazier <tombreazier@macbook-pro.lan>
2020-06-16 18:57:53 -0500

    fixed drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -128,6 +128,50 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    var tempArr = [];
+    $(this).children().each(function() {
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+  
+    var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+  
+    tempArr.push({
+      text: text,
+      date: date
+    });
+  });
+  var arrName = $(this)
+  .attr("id")
+  .replace("list-", "");
+
+// update array on tasks object and save
+  tasks[arrName] = tempArr;
+  saveTasks();
+  }
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
@@ -174,5 +218,3 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
-
